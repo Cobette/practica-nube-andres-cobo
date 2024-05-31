b9c826a7f05432a805ffaf91836aba2189b5f5de
Jurgen Zellhuber <79229146+M0nkeyroger@users.noreply.github.com>
2024-05-31 17:22:09 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html> 
+<head> 
+  <meta charset="UTF-8">
+  <title>Unitec - Cómputo en la nube</title> 
+</head> 
+<body>
+  <h1>¡Hola! </h1> 
+  <p>Primera práctica de cómputo en la nube</p> 
+</body> 
+</html>
